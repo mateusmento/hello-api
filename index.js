ab33a4314a276a591ead2673c930089c22882c2e
Mateus Sarmento <omateusmento@gmail.com>
2023-12-01 12:02:46 +0000

    Include express server with greeting endpoint

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+app.get('/:name?', async (req, res) => {
+	const personName = req.params.name;
+	res.status(200).json({
+		hello: personName ?? 'world',
+		yourId: null
+	});
+});
+
+const PORT = process.env.PORT ?? 8080;
+app.listen(PORT, () => console.log(`Listening to port ${PORT}`);
+

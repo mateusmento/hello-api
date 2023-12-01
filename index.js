bb5a67560a9d6d765f28ccfef1a82b793d0e5edb
Mateus Sarmento <omateusmento@gmail.com>
2023-12-01 12:39:50 +0000

    Include /api prefix in greeting endpoint

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-app.get('/:name?', async (req, res) => {
+app.get('/api/:name?', async (req, res) => {
 	const personName = req.params.name;
 	res.status(200).json({
 		hello: personName ?? 'world',
@@ -13,5 +13,5 @@ app.get('/:name?', async (req, res) => {
 });
 
 const PORT = process.env.PORT ?? 8080;
-app.listen(PORT, () => console.log(`Listening to port ${PORT}`);
+app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
 

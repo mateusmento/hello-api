7af52984676a08362ffbe84cfdff2ce293eee700
Mateus Sarmento <omateusmento@gmail.com>
2023-12-01 13:30:28 +0000

    Search for person in database to greet in greeting endpoint

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+const pg = require('pg');
 
 const app = express();
 app.use(cors());
 
 app.get('/api/:name?', async (req, res) => {
 	const personName = req.params.name;
-	res.status(200).json({
-		hello: personName ?? 'world',
-		yourId: null
+	const db = new pg.Client({
+		host: 'ec2-18-230-78-68.sa-east-1.compute.amazonaws.com',
+		user: 'postgres',
+		password: 'postgres',
+		database: 'postgres'
 	});
+	await db.connect();
+
+	const queryResult = await db.query('select * from person where name = $1 limit 1', [personName]);
+	const person = queryResult.rows[0];
+
+	res.status(200).json({
+		hello: person?.name ?? 'world',
+		yourId: person?.id ?? null
+	});
+
+	db.end();
 });
 
 const PORT = process.env.PORT ?? 8080;
